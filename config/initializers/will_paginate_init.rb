9fa4ff05298afaeef50f0cf6e02b403b8eb67bc7
Konstantin Makarchev <kostya27@gmail.com>
2012-09-22 01:27:54 +0400

    # dont rewrite default renderer for will_paginate

--- config/initializers/will_paginate_init.rb
+++ config/initializers/will_paginate_init.rb
@@ -3,11 +3,6 @@ require 'will_paginate/view_helpers/action_view'
 
 module WillPaginate
   module ActionView
-    def will_paginate(collection = nil, options = {})
-      options[:renderer] ||= BootstrapLinkRenderer
-      super.try :html_safe
-    end
-
     class BootstrapLinkRenderer < LinkRenderer
       protected
 
